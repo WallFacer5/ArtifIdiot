84be3b59c42b1bd21da4ec223da08ba2515c4f5f
WallFacer5 <zyh9715@126.com>
2021-03-05 00:49:43 +0800

    feat: tanh activation

--- activations.py
+++ activations.py
@@ -23,3 +23,13 @@ def sigmoid(z, direction, back_grad=0):
         return back_grad_ * forward(z_) * (1 - forward(z_))
 
     return forward(z) if direction == Directions.forward else backward(z, back_grad)
+
+
+def tanh(z, direction, back_grad=0):
+    def forward(z_):
+        return 2 * sigmoid(2 * z_, Directions.forward) - 1
+
+    def backward(z_, back_grad_):
+        return 4 * sigmoid(2 * z_, Directions.backward, back_grad_)
+
+    return forward(z) if direction == Directions.forward else backward(z, back_grad)
