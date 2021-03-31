56bf6960248de3a0bb627e68727aaa6ec089466d
WallFacer5 <zyh9715@126.com>
2021-03-31 23:41:17 +0800

    hw3

--- samples/hw3.py
+++ samples/hw3.py
@@ -1,19 +1,19 @@
 from layers.input import Input
 from layers.output import Output
 from layers.dense import Dense
-from losses import l2_loss, softmax_cross_entropy
+from losses import softmax_cross_entropy
 from session import Session
-from activations import relu, sigmoid, tanh
+from activations import relu
 from samples.download_mnist import load
-import numpy as np
-import matplotlib.pyplot as plt
-import random
+import tensorflow as tf
 
 
-def main(lr, epochs, batch_size):
+def hw3_a(lr, epochs, batch_size):
     x_train, y_train, x_test, y_test = load()
     x_train = x_train.astype(float)
     y_train = y_train.reshape([-1, 1])
+    x_test = x_test.astype(float)
+    y_test = y_test.reshape([-1, 1])
     il = Input(784)
     dl1 = Dense([il], 200, use_bias=True, activation=relu)
     dl2 = Dense([dl1], 50, use_bias=True, activation=relu)
@@ -22,3 +22,20 @@ def main(lr, epochs, batch_size):
     sess = Session([ol], x_train, y_train, x_test, y_test)
     sess.train(epochs, batch_size)
     return sess
+
+
+def hw3_b(lr, epochs, batch_size):
+    x_train, y_train, x_test, y_test = load()
+    x_train = x_train.astype(float) / 255
+    y_train = y_train.reshape([-1, 1])
+    x_test = x_test.astype(float) / 255
+    y_test = y_test.reshape([-1, 1])
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(200, use_bias=True, activation='relu'),
+        tf.keras.layers.Dense(50, use_bias=True, activation='relu'),
+        tf.keras.layers.Dense(10, use_bias=True, activation='softmax')
+    ])
+    model.compile(optimizer=tf.keras.optimizers.SGD(lr), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
+    model.evaluate(x_test, y_test)
+    return model
