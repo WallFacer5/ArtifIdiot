e59c6bbf46173f9751c797fad2215be403ee5225
WallFacer5 <zyh9715@126.com>
2021-05-14 17:09:37 +0800

    Feat: adapt numba

--- layers/conv2d.py
+++ layers/conv2d.py
@@ -23,8 +23,26 @@ class Conv2d(Layer):
         if self.use_bias:
             self.biases = bias_initializer(num_filters)
 
+    @staticmethod
     @jit(nopython=True)
+    def in_forward(cur_inputs, output_shape, filters, strides, kernel_size, use_bias, biases, activation):
+        cur_outputs = np.zeros([cur_inputs[0].shape[0]] + output_shape)
+        for i in range(output_shape[0]):
+            for j in range(output_shape[1]):
+                for k, f in enumerate(filters):
+                    for s in range(cur_inputs[0].shape[0]):
+                        cur_value = cur_inputs[0][s, i * strides[0]:i * strides[0] + kernel_size[0],
+                                    j * strides[1]:j * strides[1] + kernel_size[1]]
+                        cur_outputs[s][i][j][k] = np.sum(cur_value * f)
+                        if use_bias:
+                            cur_outputs[s][i][j][k] += biases[k]
+        if activation:
+            before_activation = np.copy(cur_outputs)
+            cur_outputs = activation(cur_outputs, Directions.forward)
+        return before_activation, cur_outputs
+
     def forward(self):
+        '''
         self.cur_outputs = np.zeros([self.cur_inputs[0].shape[0]] + self.output_shape)
         for i in range(self.output_shape[0]):
             for j in range(self.output_shape[1]):
@@ -38,11 +56,40 @@ class Conv2d(Layer):
         if self.activation:
             self.before_activation = np.copy(self.cur_outputs)
             self.cur_outputs = self.activation(self.cur_outputs, Directions.forward)
+        '''
+        self.before_activation, self.cur_outputs = self.in_forward(self.cur_inputs, self.output_shape, self.filters,
+                                                                   self.strides, self.kernel_size, self.use_bias,
+                                                                   self.biases, self.activation)
         list(map(lambda ol: ol.set_cur_input(self, self.cur_outputs), self.output_layers.keys()))
         self.clear_cur_inputs_flags()
 
+    @staticmethod
     @jit(nopython=True)
+    def in_backward(cur_inputs, output_shape, filters, strides, kernel_size, use_bias, biases, activation, cur_deltas,
+                    before_activation):
+        delta = np.sum(cur_deltas, axis=0)
+        dx = np.zeros(cur_inputs[0].shape)
+        df = np.zeros_like(filters)
+        if use_bias:
+            db = np.zeros_like(biases)
+        if activation:
+            delta = activation(before_activation, Directions.backward, delta)
+        for i in range(output_shape[0]):
+            for j in range(output_shape[1]):
+                for k, f in enumerate(filters):
+                    for s in range(cur_inputs[0].shape[0]):
+                        dx[s, i * strides[0]:i * strides[0] + kernel_size[0],
+                        j * strides[1]:j * strides[1] + kernel_size[1]] += \
+                            f * delta[s, i, j, k]
+                        if use_bias:
+                            db[k] += np.sum(delta[s, i, j, k])
+                        cur_value = cur_inputs[0][s, i * strides[0]:i * strides[0] + kernel_size[0],
+                                    j * strides[1]:j * strides[1] + kernel_size[1]]
+                        df[k] += cur_value * delta[s, i, j, k]
+        return dx, df, db
+
     def backward(self):
+        '''
         delta = np.sum(self.cur_deltas, axis=0)
         dx = np.zeros(self.cur_inputs[0].shape)
         df = np.zeros_like(self.filters)
@@ -55,13 +102,17 @@ class Conv2d(Layer):
                 for k, f in enumerate(self.filters):
                     for s in range(self.cur_inputs[0].shape[0]):
                         dx[s, i * self.strides[0]:i * self.strides[0] + self.kernel_size[0],
-                            j * self.strides[1]:j * self.strides[1] + self.kernel_size[1]] += \
+                        j * self.strides[1]:j * self.strides[1] + self.kernel_size[1]] += \
                             f * delta[s, i, j, k]
                         if self.use_bias:
                             db[k] += np.sum(delta[s, i, j, k])
                         cur_value = self.cur_inputs[0][s, i * self.strides[0]:i * self.strides[0] + self.kernel_size[0],
                                     j * self.strides[1]:j * self.strides[1] + self.kernel_size[1]]
                         df[k] += cur_value * delta[s, i, j, k]
+        '''
+        dx, df, db = self.in_forward(self.cur_inputs, self.output_shape, self.filters, self.strides, self.kernel_size,
+                                     self.use_bias, self.biases, self.activation, self.cur_deltas,
+                                     self.before_activation)
         list(map(lambda layer: layer.append_cur_delta(self, dx), self.input_layers))
         self.filters += df
         self.biases += db
--- layers/max_pool2d.py
+++ layers/max_pool2d.py
@@ -11,15 +11,31 @@ class MaxPool2d(Layer):
         self.output_shape = [input_shape[0] // pool_size[0], input_shape[1] // pool_size[1], input_shape[2]]
 
     @staticmethod
-    def get_max(cur_value):
-        max_val = np.max(cur_value)
-        for i in range(cur_value.shape[0]):
-            for j in range(cur_value.shape[1]):
-                if cur_value[i, j] == max_val:
-                    return max_val, i, j
-
     @jit(nopython=True)
+    def in_forward(output_shape, input_shapes, pool_size, cur_input):
+        def get_max(_cur_value):
+            max_val = np.max(_cur_value)
+            for i in range(_cur_value.shape[0]):
+                for j in range(_cur_value.shape[1]):
+                    if _cur_value[i, j] == max_val:
+                        return max_val, i, j
+
+        cur_outputs = np.zeros([cur_input.shape[0]] + output_shape)
+        max_flags = np.zeros([cur_input.shape[0]] + input_shapes[0])
+        for i in range(output_shape[0]):
+            for j in range(output_shape[1]):
+                for s in range(cur_input.shape[0]):
+                    for k in range(output_shape[2]):
+                        cur_value = cur_input[s,
+                                    i * pool_size[0]:i * pool_size[0] + pool_size[0],
+                                    j * pool_size[1]:j * pool_size[1] + pool_size[1], k]
+                        max_val, max_i, max_j = get_max(cur_value)
+                        cur_outputs[s, i, j, k] = max_val
+                        max_flags[s, i * pool_size[0] + max_i, j * pool_size[1] + max_j, k] = 1
+        return cur_outputs, max_flags
+
     def forward(self):
+        '''
         self.cur_outputs = np.zeros([self.cur_inputs[0].shape[0]] + self.output_shape)
         self.max_flags = np.zeros([self.cur_inputs[0].shape[0]] + self.input_shapes[0])
         for i in range(self.output_shape[0]):
@@ -32,11 +48,28 @@ class MaxPool2d(Layer):
                         max_val, max_i, max_j = self.get_max(cur_value)
                         self.cur_outputs[s, i, j, k] = max_val
                         self.max_flags[s, i * self.pool_size[0] + max_i, j * self.pool_size[1] + max_j, k] = 1
+        '''
+        self.cur_outputs, self.max_flags = self.in_forward(self.output_shape, self.input_shapes, self.pool_size,
+                                                           self.cur_inputs[0])
         list(map(lambda ol: ol.set_cur_input(self, self.cur_outputs), self.output_layers.keys()))
         self.clear_cur_inputs_flags()
 
+    @staticmethod
     @jit(nopython=True)
+    def in_backward(cur_inputs, output_shape, pool_size, max_flags, cur_deltas):
+        delta = np.zeros_like(cur_inputs[0], dtype='float64')
+        # print(np.array(self.cur_deltas).shape)
+        for i in range(output_shape[0]):
+            for j in range(output_shape[1]):
+                for s in range(cur_inputs[0].shape[0]):
+                    for k in range(output_shape[2]):
+                        delta[s, i * pool_size[0]:i * pool_size[0] + pool_size[0],
+                        j * pool_size[1]:j * pool_size[1] + pool_size[1], k] += cur_deltas[0][
+                            s][i][j][k]
+        return delta * max_flags
+
     def backward(self):
+        '''
         delta = np.zeros_like(self.cur_inputs[0], dtype='float64')
         # print(np.array(self.cur_deltas).shape)
         for i in range(self.output_shape[0]):
@@ -44,8 +77,11 @@ class MaxPool2d(Layer):
                 for s in range(self.cur_inputs[0].shape[0]):
                     for k in range(self.output_shape[2]):
                         delta[s, i * self.pool_size[0]:i * self.pool_size[0] + self.pool_size[0],
-                            j * self.pool_size[1]:j * self.pool_size[1] + self.pool_size[1], k] += self.cur_deltas[0][
+                        j * self.pool_size[1]:j * self.pool_size[1] + self.pool_size[1], k] += self.cur_deltas[0][
                             s][i][j][k]
         # print(delta * self.max_flags)
-        list(map(lambda layer: layer.append_cur_delta(self, delta * self.max_flags), self.input_layers))
+        '''
+        delta = self.in_backward(self.cur_inputs, self.output_shape, self.pool_size, self.max_flags, self.cur_deltas)
+        # list(map(lambda layer: layer.append_cur_delta(self, delta * self.max_flags), self.input_layers))
+        list(map(lambda layer: layer.append_cur_delta(self, delta), self.input_layers))
         self.clear_cur_deltas_flags()
